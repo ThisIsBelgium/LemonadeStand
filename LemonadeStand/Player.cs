0f3aaa758546f4f664e5fb2a2afd58f0a1eb2902
ThisIsBelgium <Daniel3wheeler@gmail.com>
2017-05-30 09:35:44 -0500

    Added inital classes and Player setup

---
+++ LemonadeStand/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class Player
+    {
+        private string UserName;
+        private int StartingFunds;
+        public void InitiatePlayer()
+        {
+            GetPlayerName();
+            GetStartingFunds();
+        }
+        private void GetPlayerName()
+        {
+            Console.WriteLine("Enter your Username!");
+            UserName = Console.ReadLine();
+        }
+        private void GetStartingFunds()
+        {
+            StartingFunds = 50;
+            Console.WriteLine("You start with" + " $" + StartingFunds + "\n Press enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -10,6 +10,8 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
+            Player FirstPlayer = new Player();
+            FirstPlayer.InitiatePlayer();
         }
     }
 }
